77ff6c65850f1f462c8b01029247388f67ac6b84
Colin McLeod <126152689+ColinRM000@users.noreply.github.com>
2023-02-26 02:22:29 -0500

    Update GGUI.py

--- GGUI.py
+++ GGUI.py
@@ -6,6 +6,7 @@ from oauth2client.service_account import ServiceAccountCredentials
 from tkinter import messagebox
 from ttkthemes import ThemedStyle
 from PIL import ImageTk, Image
+import customtkinter as ctk
 
 # GOOGLE DOC / SHEETS CONNECTION
 
@@ -18,69 +19,64 @@ cli = gspread.authorize(creds)
 sheet = cli.open("G-Recorder").sheet1  # Open the spreadhseet
 
 numRows = sheet.row_count  # Get the number of rows in the sheet
+
 #####################################################################################################################################
 
-root = tk.Tk()
+root = ctk.CTk()
 root.iconbitmap("GRecorder.ico")
 root.title("G-Recorder")
-root.geometry("500x300")
+root.geometry("450x400")
 
 # Configure theme colors
-bg_color = '#282828' # Dark gray background
-fg_color = '#F5F5F5' # Light gray foreground
-highlight_color = '#FFFFFF' # White highlight color
-
-root.configure(bg=bg_color)
-root.option_add('*foreground', fg_color)
-root.option_add('*background', bg_color)
-root.option_add('*highlightBackground', highlight_color)
-root.option_add('*highlightColor', fg_color)
+style = ThemedStyle(root)
+style.set_theme("arc")
+ctk.set_appearance_mode('dark')
 
 # Labels
-name_label = tk.Label(root, text="Customer Name:", bg=bg_color, fg=fg_color)
+name_label = ctk.CTkLabel(root, text="Customer Name:")
 name_label.grid(row=0, column=0, padx=10, pady=10, sticky=tk.W)
 
-email_label = tk.Label(root, text="Customer Email:", bg=bg_color, fg=fg_color)
+email_label = ctk.CTkLabel(root, text="Customer Email:")
 email_label.grid(row=1, column=0, padx=10, pady=10, sticky=tk.W)
 
-number_label = tk.Label(root, text="Customer Phone #:", bg=bg_color, fg=fg_color)
+number_label = ctk.CTkLabel(root, text="Customer Phone #:")
 number_label.grid(row=2, column=0, padx=10, pady=10, sticky=tk.W)
 
-product_label = tk.Label(root, text="Product:", bg=bg_color, fg=fg_color)
+product_label = ctk.CTkLabel(root, text="Product:")
 product_label.grid(row=3, column=0, padx=10, pady=10, sticky=tk.W)
 
-quantity_label = tk.Label(root, text="Quantity:", bg=bg_color, fg=fg_color)
+quantity_label = ctk.CTkLabel(root, text="Quantity:")
 quantity_label.grid(row=4, column=0, padx=10, pady=10, sticky=tk.W)
 
-price_label = tk.Label(root, text="Price:", bg=bg_color, fg=fg_color)
+price_label = ctk.CTkLabel(root, text="Price:")
 price_label.grid(row=5, column=0, padx=10, pady=10, sticky=tk.W)
 
 # Entry boxes
-name_entry = tk.Entry(root, bg=bg_color, fg=fg_color, insertbackground=fg_color)
+name_entry = ctk.CTkEntry(root)
 name_entry.grid(row=0, column=1)
 
-email_entry = tk.Entry(root, bg=bg_color, fg=fg_color, insertbackground=fg_color)
+email_entry = ctk.CTkEntry(root)
 email_entry.grid(row=1, column=1)
 
-number_entry = tk.Entry(root, bg=bg_color, fg=fg_color, insertbackground=fg_color)
+number_entry = ctk.CTkEntry(root)
 number_entry.grid(row=2, column=1)
 
-product_entry = tk.Entry(root, bg=bg_color, fg=fg_color, insertbackground=fg_color)
+product_entry = ctk.CTkEntry(root)
 product_entry.grid(row=3, column=1)
 
-quantity_entry = tk.Entry(root, bg=bg_color, fg=fg_color, insertbackground=fg_color)
+quantity_entry = ctk.CTkEntry(root)
 quantity_entry.grid(row=4, column=1)
 
-price_entry = tk.Entry(root, bg=bg_color, fg=fg_color, insertbackground=fg_color)
+price_entry = ctk.CTkEntry(root)
 price_entry.grid(row=5, column=1)
 
 def clear_entries():
-    name_entry.delete(0, tk.END)
-    email_entry.delete(0, tk.END)
-    number_entry.delete(0, tk.END)
-    product_entry.delete(0, tk.END)
-    quantity_entry.delete(0, tk.END)
-    price_entry.delete(0, tk.END)
+    name_entry.delete(0, ctk.END)
+    email_entry.delete(0, ctk.END)
+    number_entry.delete(0, ctk.END)
+    product_entry.delete(0, ctk.END)
+    quantity_entry.delete(0, ctk.END)
+    price_entry.delete(0, ctk.END)
 
 def record_order():
     cxname = name_entry.get()
@@ -95,18 +91,16 @@ def record_order():
     
     clear_entries() 
 
-
     messagebox.showinfo("Order Recorded", "The order has been recorded successfully!")
 
 def on_exit():
     if messagebox.askyesno("Quit", "Are you sure you want to quit?"):
         root.destroy()
 
-record_button = tk.Button(root, text="Record Order", command=record_order)
+record_button = ctk.CTkButton(root, text="Record Order", command=record_order)
 record_button.grid(row=6, column=0, columnspan=2, pady=10)
 
-quit_button = tk.Button(root, text="Quit", command=on_exit)
+quit_button = ctk.CTkButton(root, text="Quit", command=on_exit)
 quit_button.grid(row=6, column=2, columnspan=2, pady=10)
 
 root.mainloop()
-
